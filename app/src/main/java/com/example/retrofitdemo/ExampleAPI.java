e2b787bec6d90a35271f7141649f3d271a2d8d95
Cheenu <cheenusoni10@gmail.com>
2019-11-02 03:23:43 +0530

    @Query demo

--- app/src/main/java/com/example/retrofitdemo/ExampleAPI.java
+++ app/src/main/java/com/example/retrofitdemo/ExampleAPI.java
@@ -5,6 +5,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ExampleAPI {
 
@@ -13,6 +14,9 @@ public interface ExampleAPI {
     @GET("posts")     // https://jsonplaceholder.typicode.com/posts . parenthesis contains posts because relative url is /posts
     Call<List<Post>> getPosts();
 
-    @GET("posts/{id}/comments")
-    Call<List<Comment>> getComment(@Path("id")int postId);
+    /*@GET("posts/{id}/comments")
+    Call<List<Comment>> getComment(@Path("id")int postId);*/
+
+    @GET("comments")
+    Call<List<Comment>> getComment(@Query("postId")int postId);
 }
